e2146561532648986dafc3665e66e1a7ea5b6d52
Rhoopes25 <rachhoopes1@gmail.com>
2026-01-12 10:37:45 -0700

    added in DiceRoller function

---
+++ ConsoleApp1/DiceRoller.cs
@@ -0,0 +1,24 @@
+using System;
+namespace ConsoleApp1;
+
+public class DiceRoller
+{
+    public int[] RollDice(int rolls)
+    {
+        // dice roll will be between 2-12
+        int[] results = new int[13];
+        // creats a random number
+        Random random = new Random();
+
+        for (int i = 0; i < rolls; i++)
+        {
+            int die1 = random.Next(1, 7);
+            int die2 = random.Next(1, 7);
+            int sum = die2 + die1;
+            
+            //add count to number of times number was rolled
+            results[sum]++;
+        }
+        return results;
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using ConsoleApp1;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("My name is Rachel!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.Write("How many dice rolls would you like to simulate? ");
+        int rolls = int.Parse(System.Console.ReadLine());
+        
+        //where we pass number of times to roll into diceRoller and get dice rolls back
+        DiceRoller diceRoller = new DiceRoller();
+        
+        // get the array of dice roll results
+        int[] results = diceRoller.RollDice(rolls);
+
+            // get the array of dice rolls
+            // print dice rolls
+    }
+}
